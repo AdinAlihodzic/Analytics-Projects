7945b99f55ad968971c69df05629b45ad61ab641
AdinAlihodzic <adin.alihodzic@finsure.com.au>
2023-12-14 08:57:08 +1100

    main

---
+++ NextGen_Data_Modelling.sql
@@ -0,0 +1,39 @@
+--- the below code creates a simplified data model of nextgen data 
+--- the purpose of this dtaa model is to be able to take point in time snapshot view of all customers that have had their applications lodged via applyonline 
+
+
+
+
+-- List all applications
+
+select top 100 * from Nextgen_Prod.dbo.Application
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--- who are all the customers in nextgen -- in this case we want to focus on individuals only
+
+
+select * from 
+
+
+
+
+
